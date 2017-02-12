724181b1bc7c00c4a9406792c93d00776d49a793
JL <jldeleage@free.fr>
2017-02-12 08:58:45 +0100

    Bug with dialect in velocity action and sample project
    The dialect object was overridden by the name of its class in the
    velocity startup. The bug is fixed now.
    The Java dialect is used in the sample project to handle the date type.
    The Insset-Airways project has been enhanced to compile and package the
    generated sources.

---
+++ samples/insset_airways/src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,55 @@
+
+
+import static fr.insset.jeanluc.ete.api.ActionSupport.MODEL;
+import fr.insset.jeanluc.ete.api.EteException;
+import fr.insset.jeanluc.ete.api.impl.VelocityAction;
+import fr.insset.jeanluc.ete.meta.model.mofpackage.EteModel;
+import fr.insset.jeanluc.ete.meta.model.mofpackage.MofPackage;
+import fr.insset.jeanluc.ete.meta.model.mofpackage.impl.EteModelImpl;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+/**
+ *
+ * @author jldeleage
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() throws EteException {
+        VelocityAction velocityAction = new VelocityAction();
+        velocityAction.addParameter("x", "Hellow world");
+        EteModel model = new EteModelImpl();
+        velocityAction.addParameter(MODEL, model);
+//        MofPackage process = velocityAction.process(model);
+     }
+
+}
